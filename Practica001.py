c4c62ab8916e9dff9415743788f4a6703905df34
Martín Javier Del Percio <martinjavierd@gmail.com>
2018-05-07 15:02:39 -0300

    Casos para la Web y para GUI Windows

--- Practica001.py
+++ Practica001.py
@@ -3,21 +3,17 @@ import pyautogui
 import subprocess
 
 # Abre la Calculadora
-
 cmd = "C:/Windows/System32/calc.exe"
 process = subprocess.Popen(cmd, stdout=subprocess.PIPE, creationflags=0x08000000)
 
 # Pausa de rigor
-
 pyautogui.PAUSE = 2
 
 # Funcion <pulsarBoton>
 # Verifica si existe un elemento en pantalla
 # Si existe, toma sus coordenadas y le hace click
-
 def pulsarBoton(objeto):
     ObjName = "images\\" + objeto + '.PNG'
-
     SiExiste = pyautogui.locateOnScreen(ObjName)
     print(SiExiste)
     button7x, button7y = pyautogui.center(SiExiste)
